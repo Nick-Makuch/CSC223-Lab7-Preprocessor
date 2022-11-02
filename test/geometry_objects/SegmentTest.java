edd0387d3c853a3c8a1003a1b70463411fd6ec36
emil-westling <westling01@gmail.com>
2022-11-02 16:29:17 -0400

    skeleton SegmentTest class

---
+++ test/geometry_objects/SegmentTest.java
@@ -0,0 +1,36 @@
+package geometry_objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import geometry_objects.Segment;
+import geometry_objects.points.Point;
+import geometry_objects.points.PointDatabase;
+import input.InputFacade;
+import input.components.FigureNode;
+import preprocessor.delegates.ImplicitPointPreprocessor;
+
+public class SegmentTest {
+
+	@Test
+	void HasSubSegmentTest() {
+		
+	}
+	
+	@Test
+	void CoincideWithoutOverlapTest() {
+		
+	}
+	
+	@Test
+	void collectOrderedPointsOnSegmentsTest() {
+		
+	}
+}
