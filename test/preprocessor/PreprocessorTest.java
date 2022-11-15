94d9fa88a02950f23ad0e322eddd219526a42ae9
emil-westling <westling01@gmail.com>
2022-11-14 19:39:44 -0500

    tests

--- test/preprocessor/PreprocessorTest.java
+++ test/preprocessor/PreprocessorTest.java
@@ -165,24 +165,24 @@ class PreprocessorTest
 	}
 	
 	@Test
-	void testComputeImplicitBaseSegments() {
+	void testComputeImplicitBaseSegmentsAndIdentidyMinSeg() {
 		//
-		//          D
-		//		   /   
-		//   A ---/------- B
-		//       /
-		//    E-/-----F
-		//	   / 
-		//    C
+		//          D (3,0)
+		//		    |  
+		//   A -----|----- B(6,2)
+		//          |
+		//    E-----|------F(6,3)
+		//	        |
+		//    		C (3,5)
 		//
 		Point A = new Point("A", 0,2);
 		Point B = new Point("B", 6, 2);
-		Point C = new Point("C", 1, 5);
+		Point C = new Point("C", 3, 5);
 		Point D = new Point("D",  3, 0);
-		Point E = new Point("E", 1, 3);
+		Point E = new Point("E", 0, 3);
 		Point F = new Point("F", 6, 3);
-		Point E_star = new Point("*_E", 9.0/5, 3);
-		Point A_star = new Point("*_A", 13.0/5, 3);
+		Point E_star = new Point("*_E", 3, 3);
+		Point A_star = new Point("*_A", 3, 2);
 		
 	
 		List<Point> ptList = new ArrayList<Point>();
@@ -206,32 +206,38 @@ class PreprocessorTest
 		impPoints.add(E_star);
 		impPoints.add(A_star);
 		
-		Segment AA_star = new Segment(A, E_star);
+		Segment AA_star = new Segment(A, A_star);
 		Segment BA_star = new Segment(B, A_star);
 		Segment DA_star = new Segment(D, A_star);
 		Segment CE_star = new Segment(C, E_star);
 		Segment EE_star = new Segment(E, E_star);
 		Segment FE_star = new Segment(F, E_star);
-	
-		Set<Segment> expImpSeg = new HashSet<Segment>();
-		expImpSeg.add(FE_star);
-		expImpSeg.add(EE_star);
-		expImpSeg.add(CE_star);
-		expImpSeg.add(DA_star);
-		expImpSeg.add(BA_star);
-		expImpSeg.add(AA_star);
-		
-		
-		
+		Segment EA_star = new Segment(A_star, E_star);
 		
 		Preprocessor pp = new Preprocessor(ptdb, segSet);
 		Set<Segment> impSeg = pp.computeImplicitBaseSegments(impPoints);
 		
-		assertTrue(impSeg.contains(expImpSeg));
+		assertTrue(impSeg.contains(AA_star));
+		assertTrue(impSeg.contains(BA_star));
+		assertTrue(impSeg.contains(DA_star));
+		assertTrue(impSeg.contains(CE_star));
+		assertTrue(impSeg.contains(EE_star));
+		assertTrue(impSeg.contains(FE_star));
+		assertTrue(impSeg.contains(EA_star));
+		assertEquals(7, impSeg.size());
 		
+		
+		Set<Segment> minSeg = pp.identifyAllMinimalSegments(impPoints, segSet, impSeg);
+		assertTrue(minSeg.contains(AA_star));
+		assertTrue(minSeg.contains(BA_star));
+		assertTrue(minSeg.contains(DA_star));
+		assertTrue(minSeg.contains(CE_star));
+		assertTrue(minSeg.contains(EE_star));
+		assertTrue(minSeg.contains(FE_star));
+		assertTrue(minSeg.contains(EA_star));
+		assertEquals(7, minSeg.size());
 	}
-	
-	
+
 	@Test
 	void testConstructAllNonMinimalSegments_withTwoSegments() 
 	{
